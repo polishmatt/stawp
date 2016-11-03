3135c48a5271ea06af4f470398b12f084ef570ab
Matt Wisniewski <hello@mattw.us>
2016-11-03 00:31:13 -0400

    move gallery code to a module

---
+++ modules/gallery.py
@@ -0,0 +1,183 @@
+
+import os
+import yaml
+from PIL import Image
+
+TEMPLATES = {}
+
+rendered_galleries = {}
+rendered_gallery = None
+
+# should be options
+discover = True
+clear = True
+
+def __init__(base_path, config):
+    for template in ['category', 'gallery', 'image']:
+        file = open('%s/html/gallery/%s.html' % (base_path, template), 'r')
+        TEMPLATES[template] = file.read()
+        file.close()
+
+    for page in config['bottomMenu']:
+        rendered_galleries[page] = None
+
+def interpret_config(page, config, source_path, source, file_name, default, configPage, children, parents, index, bodyhtml):
+    if 'images' in page:
+        changed = False
+        if discover:
+            for child in children:
+                try:
+                    Image.open(os.path.join(source, child))
+                    child = child.split('/')
+                    child = child[len(child)-1]
+                    dupe = False
+                    for image in page['images']:
+                        if child == image or isinstance(image, dict) and child == list(image.keys())[0]:
+                            dupe = True
+                    if not dupe:
+                        page['images'].append(child)
+                        default['images'].append(child)
+                        changed = True
+                except:
+                    pass
+
+        imageHTML = ''
+        page['pageTitle'] = page['title']
+        outPrefix = 'matt-wisniewski-' + page['dirName'] + '-'
+
+        if page['body'] != '':
+            page['body'] = bodyhtml.replace('{{body}}', page['body'])
+        body = TEMPLATES['gallery'].replace('{{title}}', page['pageTitle'])
+        body = body.replace('{{body}}', page['body'])
+
+        if isinstance(page['images'][0], list):
+            images = []
+            for lists in page['images']:
+                images = images + lists
+                images.append({'n':None})
+            del images[-1]
+            page['images'] = images
+
+        for image in page['images']:
+            ori = image
+            title = None
+            if isinstance(image, dict):
+                path = list(image.keys())[0]
+                if path == 'n':
+                    imageHTML += '</div><div class="gallery">'
+                elif '.' in path and 'isCategory' not in page:
+                    title = image[path]
+                    image = path
+                else:
+                    image = image[path]
+                    if '/'in path or image is not None and '/' in image and '.com' not in image:
+                        categoryPath = path + '/'
+                        outPrefix = ''
+                        if '/' in path:
+                            ps = path.split('/')
+                            outPrefix = ''
+                            image = 'matt-wisniewski-' + ps[len(ps)-1] + '-' + image
+                    else:
+                        categoryPath = file_name[2:] + '/' + path + '/'
+                        outPrefix = 'matt-wisniewski-'+ path + '-'
+                    if image is not None and '/' in image and '.com' not in image:
+                        image = image.split('/')
+                        outPrefix = 'matt-wisniewski-'+ image[0] + '-'
+                        image = image[1]
+                    body = TEMPLATES['category'].replace('{{title}}', page['pageTitle'])
+                    body = body.replace('{{body}}', page['body'])
+                    page['isCategory'] = True
+
+                    if '.' in path and '..' not in path:
+                        attitle = ' '.join([s.capitalize() for s in path[22:-4].split('-')])
+                        alt = page['pageTitle'] + ' - ' + attitle
+                        icfg = {
+                            'href': 'https://'+image,
+                            'src': config['path']+file_name[2:]+'/'+path,
+                            'alt': alt,
+                            'title': attitle,
+                        }
+                        ichtml = TEMPLATES['image']
+                        for key in icfg:
+                            ichtml = ichtml.replace('{{%s}}' % key, icfg[key])
+                        imageHTML += ichtml
+                    else:
+                        file = open(source+'/' + categoryPath+'index.yaml', 'r')
+                        cfg = file.read()
+                        file.close()
+                        cfg = yaml.load(cfg)
+                        attitle = cfg['title']
+                        alt = page['pageTitle'] + ' - ' + attitle
+                        icfg = {
+                            'href': config['path']+categoryPath,
+                            'src': config['path']+categoryPath+'thumb-'+outPrefix+image,
+                            'alt': alt,
+                            'title': attitle,
+                        }
+                        ichtml = TEMPLATES['image']
+                        for key in icfg:
+                            ichtml = ichtml.replace('{{%s}}' % key, icfg[key])
+                        imageHTML += ichtml
+                    page['categoryTitle'] = ''
+            else:
+                name = image.split('.')[0]
+                cat = file_name.split('/')[-2]
+                if name.isdigit() and cat != 'collaborations':
+                    title = page['title'] + ' #' + name
+            if 'isCategory' not in page:
+                if not clear or os.path.isfile(os.path.join(source_path, image)) and default['images'].count(image) < 2:
+                    if parents[file_name] == index:
+                        alt = ''
+                    else:
+                        alt = parents[file_name]['pageTitle']
+                    alt += ' - '
+                    if title is not None:
+                        alt += title
+                    else:
+                        alt += page['title']
+                        title = ''
+                    icfg = {
+                        'href': outPrefix+image,
+                        'src': 'thumb-'+outPrefix+image,
+                        'alt': alt,
+                        'title': title,
+                    }
+                    ichtml = TEMPLATES['image'] 
+                    for key in icfg:
+                        ichtml = ichtml.replace('{{%s}}' % key, icfg[key])
+                    imageHTML += ichtml
+                    if parents[file_name] != index:
+                        page['categoryTitle'] = ''
+                        # parents[file_name]['pageTitle']
+                    if 'childDescription' in parents[file_name]:
+                        page['description'] = parents[file_name]['childDescription'].replace('{{title}}', page['title'])
+                    page['isGallery'] = True
+                else:
+                    print(os.path.join(source_path, image))
+                    default['images'].remove(ori)
+                    changed = True
+
+        page['images'] = default['images']
+        if changed:
+            file = open(source_path + '/index.yaml', 'w')
+            file.write(yaml.dump(default))
+            file.close()
+        
+        body = body.replace('{{images}}', imageHTML)
+        page['body'] = body
+        if 'isCategory' in page and page['isCategory'] and configPage in config['bottomMenu']:
+            rendered_galleries[configPage] = page['body']
+        else:
+            if page['categoryTitle'] != '':
+                page['title'] = page['categoryTitle'] + ' - ' + page['title']
+    else:
+        page['pageTitle'] = ''
+        page['categoryTitle'] = ''
+
+def render_page(page, config):
+    global rendered_gallery
+    if rendered_gallery is None:
+        rendered_gallery = ''.join(rendered_galleries[page] for page in config['bottomMenuPages'])
+
+    page['body'] = page['body'].replace('{{gallery}}', rendered_gallery)
+
--- swp.py
+++ swp.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 
 import sys
-import json
+import os
 from os import listdir, remove, mkdir, rename
-from os.path import isdir, isfile, join
+from os.path import isdir, join
 import shutil
 import yaml
 from PIL import Image
 import copy
+import imp
 from distutils.dir_util import copy_tree
+
+module_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'modules')
+module = imp.load_source('swp_module_gallery', os.path.join(module_path, 'gallery.py'))
 
 dist = sys.argv[2]
 if dist is None or dist == "":
@@ -21,15 +25,6 @@ config = file.read()
 config = yaml.load(config)
 file.close()
 
-file = open(base + '/html/gallery/category.html', 'r')
-category = file.read()
-file.close()
-file = open(base + '/html/gallery/gallery.html', 'r')
-exhibit = file.read()
-file.close()
-file = open(base + '/html/gallery/image.html', 'r')
-ihtml = file.read()
-file.close()
 file = open(base+ '/html/body.html', 'r')
 bodyhtml = file.read()
 file.close()
@@ -41,13 +36,13 @@ pages = []
 index = None
 topLevel = []
 parents = {}
-gallery = {}
 config['css'] = 1
 config['js'] = 1
 config['bottomMenu'] = config['menu'][1]
 config['topMenu'] = config['menu'][0]
-for page in config['bottomMenu']:
-    gallery[page] = None
+
+module.__init__(base_path=base, config=config)
+
 topMenu = {}
 bottomMenu = {}
 sitemap = [{
@@ -57,10 +52,6 @@ sitemap = [{
     'url': 'images/',
     'priority': '0.8',
 }]
-
-# should be options
-discover = True
-clear = True
 
 while dirs:
     nextDirs = []
@@ -111,157 +102,8 @@ while dirs:
 
             page['header'] = ''
             page['categoryTitle'] = ''
-            if 'images' in page:
-                changed = False
-                if discover:
-                    for child in children:
-                        try:
-                            Image.open(join(source, child))
-                            child = child.split('/')
-                            child = child[len(child)-1]
-                            dupe = False
-                            for image in page['images']:
-                                if child == image or isinstance(image, dict) and child == list(image.keys())[0]:
-                                    dupe = True
-                            if not dupe:
-                                page['images'].append(child)
-                                default['images'].append(child)
-                                changed = True
-                        except:
-                            pass
 
-                imageHTML = ''
-                page['pageTitle'] = page['title']
-                outPrefix = 'matt-wisniewski-' + page['dirName'] + '-'
-
-                if page['body'] != '':
-                    page['body'] = bodyhtml.replace('{{body}}', page['body'])
-                body = exhibit.replace('{{title}}', page['pageTitle'])
-                body = body.replace('{{body}}', page['body'])
-
-                if isinstance(page['images'][0], list):
-                    images = []
-                    for lists in page['images']:
-                        images = images + lists
-                        images.append({'n':None})
-                    del images[-1]
-                    page['images'] = images
-
-                for image in page['images']:
-                    ori = image
-                    title = None
-                    if isinstance(image, dict):
-                        path = list(image.keys())[0]
-                        if path == 'n':
-                            imageHTML += '</div><div class="gallery">'
-                        elif '.' in path and 'isCategory' not in page:
-                            title = image[path]
-                            image = path
-                        else:
-                            image = image[path]
-                            if '/'in path or image is not None and '/' in image and '.com' not in image:
-                                categoryPath = path + '/'
-                                outPrefix = ''
-                                if '/' in path:
-                                    ps = path.split('/')
-                                    outPrefix = ''
-                                    image = 'matt-wisniewski-' + ps[len(ps)-1] + '-' + image
-                            else:
-                                categoryPath = fileName[2:] + '/' + path + '/'
-                                outPrefix = 'matt-wisniewski-'+ path + '-'
-                            if image is not None and '/' in image and '.com' not in image:
-                                image = image.split('/')
-                                outPrefix = 'matt-wisniewski-'+ image[0] + '-'
-                                image = image[1]
-                            body = category.replace('{{title}}', page['pageTitle'])
-                            body = body.replace('{{body}}', page['body'])
-                            page['isCategory'] = True
-
-                            if '.' in path and '..' not in path:
-                                attitle = ' '.join([s.capitalize() for s in path[22:-4].split('-')])
-                                alt = page['pageTitle'] + ' - ' + attitle
-                                icfg = {
-                                    'href': 'https://'+image,
-                                    'src': config['path']+fileName[2:]+'/'+path,
-                                    'alt': alt,
-                                    'title': attitle,
-                                }
-                                ichtml = ihtml
-                                for key in icfg:
-                                    ichtml = ichtml.replace('{{%s}}' % key, icfg[key])
-                                imageHTML += ichtml
-                            else:
-                                file = open(source+'/' + categoryPath+'index.yaml', 'r')
-                                cfg = file.read()
-                                file.close()
-                                cfg = yaml.load(cfg)
-                                attitle = cfg['title']
-                                alt = page['pageTitle'] + ' - ' + attitle
-                                icfg = {
-                                    'href': config['path']+categoryPath,
-                                    'src': config['path']+categoryPath+'thumb-'+outPrefix+image,
-                                    'alt': alt,
-                                    'title': attitle,
-                                }
-                                ichtml = ihtml
-                                for key in icfg:
-                                    ichtml = ichtml.replace('{{%s}}' % key, icfg[key])
-                                imageHTML += ichtml
-                            page['categoryTitle'] = ''
-                    else:
-                        name = image.split('.')[0]
-                        cat = fileName.split('/')[-2]
-                        if name.isdigit() and cat != 'collaborations':
-                            title = page['title'] + ' #' + name
-                    if 'isCategory' not in page:
-                        if not clear or isfile(join(sourcePath, image)) and default['images'].count(image) < 2:
-                            if parents[fileName] == index:
-                                alt = ''
-                            else:
-                                alt = parents[fileName]['pageTitle']
-                            alt += ' - '
-                            if title is not None:
-                                alt += title
-                            else:
-                                alt += page['title']
-                                title = ''
-                            icfg = {
-                                'href': outPrefix+image,
-                                'src': 'thumb-'+outPrefix+image,
-                                'alt': alt,
-                                'title': title,
-                            }
-                            ichtml = ihtml
-                            for key in icfg:
-                                ichtml = ichtml.replace('{{%s}}' % key, icfg[key])
-                            imageHTML += ichtml
-                            if parents[fileName] != index:
-                                page['categoryTitle'] = ''
-                                # parents[fileName]['pageTitle']
-                            if 'childDescription' in parents[fileName]:
-                                page['description'] = parents[fileName]['childDescription'].replace('{{title}}', page['title'])
-                            page['isGallery'] = True
-                        else:
-                            print(join(sourcePath, image))
-                            default['images'].remove(ori)
-                            changed = True
-
-                page['images'] = default['images']
-                if changed:
-                    file = open(sourcePath + '/index.yaml', 'w')
-                    file.write(yaml.dump(default))
-                    file.close()
-                
-                body = body.replace('{{images}}', imageHTML)
-                page['body'] = body
-                if 'isCategory' in page and page['isCategory'] and configPage in config['bottomMenu']:
-                    gallery[configPage] = page['body']
-                else:
-                    if page['categoryTitle'] != '':
-                        page['title'] = page['categoryTitle'] + ' - ' + page['title']
-            else:
-                page['pageTitle'] = ''
-                page['categoryTitle'] = ''
+            module.interpret_config(page, config, sourcePath, source, fileName, default, configPage, children, parents, index, bodyhtml)
 
             for child in children:
                 parents[child] = page
@@ -303,8 +145,6 @@ config['menu[1]'] = config['bottomMenu']
 for key in config:
     template = template.replace('{{%s}}' % key, str(config[key]))
 
-gallery = ''.join(gallery[page] for page in config['bottomMenuPages'])
-
 site = '<?xml version="1.0" encoding="UTF-8"?>'+"\n"
 site += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'+"\n"
 for url in sitemap:
@@ -325,7 +165,7 @@ for page in pages:
         pass
     output = template
 
-    page['body'] = page['body'].replace('{{gallery}}', gallery)
+    module.render_page(page=page, config=config)
     if 'description' in page:
         page['description'] = ' ' + page['description']
     else:
