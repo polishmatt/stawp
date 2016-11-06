b54e38e971b52b3a91922f1ecb494917d95cf9aa
Matt Wisniewski <hello@mattw.us>
2016-11-06 00:09:45 -0400

    add tests

--- module.py
+++ module.py
@@ -1,5 +1,6 @@
 
 class Module:
+
     def __init__(self, builder):
         pass
 
---
+++ tests/test_build.py
@@ -0,0 +1,20 @@
+
+import unittest
+import filecmp
+import build
+
+class TestBuild(unittest.TestCase):
+
+    def build_fixture(self, name):
+        dest='/tmp/swp'
+        source='tests/fixtures/%s/src' % name
+        builder = build.Builder(dist=dest, base=source)
+        builder.interpret()
+        builder.render()
+        cmp = filecmp.dircmp(dest, 'tests/fixtures/%s/dest' % name)
+        for attr in ['left_only', 'right_only', 'common_funny', 'diff_files', 'funny_files']:
+            self.assertEqual(len(getattr(cmp, attr)), 0, attr + " - " + str(getattr(cmp, attr)))
+
+    def test_build_index(self):
+        self.build_fixture('index')
+
