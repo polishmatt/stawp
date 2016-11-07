d0174f21acedcf65e44586c63078a59ef6863b9b
Matt Wisniewski <hello@mattw.us>
2016-11-06 19:51:53 -0500

    add tests for default behavior

--- tests/test_build.py
+++ tests/test_build.py
@@ -5,7 +5,7 @@ import classes.build
 
 class TestBuild(unittest.TestCase):
 
-    def build_fixture(self, name):
+    def build_fixture(self, name, description):
         dest='/tmp/swp'
         source='tests/fixtures/%s/src' % name
         builder = classes.build.Builder(dist=dest, base=source)
@@ -13,8 +13,14 @@ class TestBuild(unittest.TestCase):
         builder.render()
         cmp = filecmp.dircmp(dest, 'tests/fixtures/%s/dest' % name)
         for attr in ['left_only', 'right_only', 'common_funny', 'diff_files', 'funny_files']:
-            self.assertEqual(len(getattr(cmp, attr)), 0, attr + " - " + str(getattr(cmp, attr)))
+            self.assertEqual(len(getattr(cmp, attr)), 0, 'Failed ' + description + "\n" + attr + " - " + str(getattr(cmp, attr)))
 
-    def test_build_index(self):
-        self.build_fixture('index')
+    def test_build_min_copy(self):
+        self.build_fixture('minimum-copy', 'copy file only')
+
+    def test_build_min_page(self):
+        self.build_fixture('minimum-page', 'minimum configuration with one page')
+
+    def test_build_default(self):
+        self.build_fixture('builder', 'default build behavior')
 
